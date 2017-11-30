dffc7b158358d4421f5d22a84619b3db9d00d228
Michael Pfeifer <mikepf97@gmail.com>
2017-11-30 16:08:32 -0700

    Tinkered with assembly file

--- temp.asm
+++ temp.asm
@@ -1,25 +1,25 @@
 nop
-ld   dir     018
-st   idx     018
-em   xdr     018
-ldx  imm x1  12
-stx  dir x2  018
-emx  dir x3  018
-add  idx     018
-sub  xdr     018
-clr
-com
-and  dir     018
-or   xdr     018
-xor  imm     57
+ld   dir     016
 addx dir x1  017
 subx imm x0  345
 clrx     x1
-j    dir 015 018
+em   xdr     018
+ldx  imm x1  13
+stx  dir x2  016
+emx  dir x3  017
+add  idx     016
+sub  xdr     018
+clr
+com
+and  dir     016
+or   xdr     018
+xor  imm     57
+st   idx     017
+j    dir 015 017
 jz   idx 0c4 018
-jn   idr 0c4 018
-jp   xdr 0c4 018
+jn   idr 0c4 016
+jp   xdr 0c4 016
 halt
-100
-1000
-10000
+8
+10
+243
